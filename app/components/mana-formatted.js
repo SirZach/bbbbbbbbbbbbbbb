c5f1feeb71b7f8b616bc23f94883a0eb9a4c38fb
sirzach <zach.mcgonigal@gmail.com>
2015-02-13 22:26:49 -0500

    wired up mana symbol fonts

---
+++ app/components/mana-formatted.js
@@ -0,0 +1,54 @@
+import Ember from 'ember';
+
+function getSymbol (letter) {
+  if (letter === 'U') {
+    return {
+      symbol: '+',
+      description: 'Island'
+    };
+  } else if (letter === 'B') {
+    return {
+      symbol: '=',
+      description: 'Swamp'
+    };
+  } else if (letter === 'R') {
+    return {
+      symbol: '<',
+      description: 'Mountain'
+    };
+  } else if (letter === 'G') {
+    return {
+      symbol: '>',
+      description: 'Forest'
+    };
+  } else if (letter === 'W'){
+    return {
+      symbol: '@',
+      description: 'Plains'
+    };
+  } else if (!isNaN(letter)) {
+    return {
+      isNumber: true,
+      value: letter
+    };
+  } else {
+    return {
+      noSymbol: true,
+      value: letter
+    };
+  }
+}
+
+export default Ember.Component.extend({
+  formattedCharacters: function () {
+    var mana = this.get('mana'),
+        charObjects = [];
+
+    for (var i = 0; i < mana.length; i++) {
+      var char = mana.charAt(i);
+      charObjects.pushObject(getSymbol(char));
+    }
+
+    return charObjects;
+  }.property('mana')
+});
