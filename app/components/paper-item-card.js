41fa048bb669c54a37fb82eb66550560d92990aa
sirzach <zach.mcgonigal@gmail.com>
2015-02-24 21:18:17 -0500

    getting rid of no longer needed component

--- app/components/paper-item-card.js
+++
@@ -1,9 +0,0 @@
-import PaperItem from 'ember-paper/components/paper-item';
-
-export default PaperItem.extend({
-
-  click: function () {
-    var card = this.get('card');
-    this.sendAction('action', card);
-  }
-});
