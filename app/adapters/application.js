8de4dd641c47149cccf676ca29c6dd5c771b7fda
sirzach <zach.mcgonigal@gmail.com>
2015-03-29 11:10:11 -0400

    adding the real api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
+import ENV from 'webatrice/config/environment';
+var host;
+
+if (ENV.environment === 'production') {
+  host = 'https://big-furry-monster.herokuapp.com';
+} else {
+  host = 'http://localhost:3000';
+}
 
 export default DS.RESTAdapter.extend({
-  host: 'http://localhost:3000'
+  host: host
 });
