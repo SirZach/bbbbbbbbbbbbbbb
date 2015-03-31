39a08c2b7c6dce7a9da7d374558c3bc3c74ece39
sirzach <zach.mcgonigal@gmail.com>
2015-03-30 21:50:00 -0400

    initial firebase integration

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'webatrice',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://dazzling-fire-7827.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
