938d4bac391fabae7d0f10d985c37f2679a4627e
sirzach <zach.mcgonigal@gmail.com>
2015-04-01 00:35:37 -0400

    retrieve your list of decks you've created

--- app/routes/decks.js
+++ app/routes/decks.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function () {
-    if (!this.get('isAuthenticated')) {
+    if (!this.get('session.isAuthenticated')) {
       this.replaceWith('/');
     }
   },
 
   model: function () {
     return this.store.find('deck', {
-      orderBy: 'name',
+      orderBy: 'owner',
       equalTo: this.get('session.currentUser.github.username')
     });
   }
