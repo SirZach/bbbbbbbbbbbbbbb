d6b154f0270ebdbb6a1e8fb0c02492a32e85d5c7
ahaurw01 <aaron.haurwitz@gmail.com>
2015-03-31 20:55:30 -0400

    have a chat

---
+++ app/models/chat.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  // user: DS.belongsTo('user'),
+  channel: DS.attr('string'),
+  when: DS.attr('date'),
+  says: DS.attr('string'),
+  // Until the user relationship is wired up.
+  name: DS.attr('string'),
+  avatarUrl: DS.attr('string')
+});
