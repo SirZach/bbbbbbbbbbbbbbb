537bec8b3c43393697ff1c3ea2b34f049ef971df
ahaurw01 <aaron.haurwitz@gmail.com>
2015-04-03 23:50:47 -0400

    keep it rolling with idle tracking

--- app/components/user-status.js
+++ app/components/user-status.js
@@ -9,5 +9,18 @@ export default Ember.Component.extend({
   statusClass: function () {
     var state = this.get('presence.state');
     return 'chat-' + state;
+  }.property('presence.state'),
+
+  isActive: Ember.computed.equal('presence.state', 'online'),
+
+  isOffline: Ember.computed.equal('presence.state', 'offline'),
+
+  lastSeenLabel: function () {
+    var state = this.get('presence.state');
+    if (state === 'idle') {
+      return 'idle for';
+    } else if (state === 'offline') {
+      return 'last seen';
+    }
   }.property('presence.state')
 });
--- app/helpers/from-now.js
+++ app/helpers/from-now.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
-export function fromNow(params) {
-  return moment(params[0]).fromNow();
+export function fromNow(params, options) {
+  return moment(params[0]).fromNow(!options.includeSuffix);
 }
 
 export default Ember.HTMLBars.makeBoundHelper(fromNow);
