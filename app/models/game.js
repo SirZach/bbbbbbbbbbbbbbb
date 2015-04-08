269bbc25afe280ad1c29cf5e6a05ccbb36edbf8d
sirzach <zach.mcgonigal@gmail.com>
2015-04-08 00:35:00 -0400

    requested when i loaded the app

--- app/models/game.js
+++ app/models/game.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   createdDate: DS.attr('date'),
   watchers: DS.hasMany('user'),
-  players: DS.hasMany('user')
+  players: DS.hasMany('user', {async: true})
   // state: DS.attr()
 });
