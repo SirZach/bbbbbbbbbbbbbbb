b6a99c288ee64e9d8a0a0f64972cff764e2bb5e1
ahaurw01 <aaron.haurwitz@gmail.com>
2015-04-11 19:50:10 -0400

    Give game a status property that is more than just a boolean

--- app/models/game.js
+++ app/models/game.js
@@ -1,7 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  /** @property {Date} When this game was created. */
   createdDate: DS.attr('date'),
+
+  /** @property {Array<GameParticipant>} Embedded participant records. */
   gameParticipants: DS.hasMany('gameParticipant', {embedded: true}),
-  isStarted: DS.attr('boolean', {defaultValue: false})
+
+  /** @property {String} One of 'preparing', 'in-play', 'ended'. */
+  status: DS.attr('string', {defaultValue: 'preparing'})
 });
