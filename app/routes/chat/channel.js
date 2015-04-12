ba49fa31435d5b9cc90ea8417ea841dbbcbdea93
ahaurw01 <aaron.haurwitz@gmail.com>
2015-04-12 14:55:07 -0400

    quick fix for weird presences showing up in the lobby person list

--- app/routes/chat/channel.js
+++ app/routes/chat/channel.js
@@ -23,9 +23,9 @@ export default Ember.Route.extend({
   setupController: function (controller, model) {
     controller.set('channel', this.get('channel'));
     controller.set('presences');
-    this.store.find('presence').then(function (presences) {
-      controller.set('presences', presences);
-    });
+    var presences = this.store.filter('presence', {},
+      (presence) => !!presence._data.user);
+    controller.set('presences', presences);
     return this._super(controller, model);
   },
 
