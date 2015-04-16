d801ea8031aeb9be9a80a633c66757ef6122a05c
ahaurw01 <aaron.haurwitz@gmail.com>
2015-04-15 23:00:33 -0400

    get cooking with game prep display logic

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -15,14 +15,111 @@ export default Ember.Controller.extend({
     var playerUserIds = this.get('model.players').mapBy('user.id');
     var myId = this.get('session.user.id');
     return playerUserIds.contains(myId);
-  }.property('model.players.@each.user.id', 'session.user.id'),
+  }.property('model.players.@each.user', 'session.user.id'),
 
   /** @property {GameParticipant} The participant representing me. */
   participant: function () {
     var participants = this.get('model.gameParticipants');
     var myId = this.get('session.user.id');
     return participants.findBy('user.id', myId);
-  }.property('model.gameParticipants.@each.user.id', 'session.user.id'),
+  }.property('model.gameParticipants.@each.user', 'session.user.id'),
+
+  /**
+   * @property {Array<GameParticipant>} The participants playing the game. The
+   *                                    first player will be me if I am playing.
+   *                                    It will be null if I am not playing and
+   *                                    there is only one other player.
+   */
+  players: function () {
+    var players = this.get('model.gameParticipants').filterBy('isPlaying');
+    var amIPlaying = this.get('amIPlaying');
+    var myId = this.get('session.user.id');
+    if (!amIPlaying) {
+      if (players.length > 1) {
+        // Simply sort by username.
+        return players.sortBy('user.username');
+      } else {
+        // Make the first player null and the other player the second player.
+        return [null].concat(players);
+      }
+    } else {
+      // Make sure I am the first player.
+      return [this.get('participant')]
+        .concat(players.rejectBy('user.id', myId));
+    }
+  }.property('model.gameParticipants.@each.isPlaying', 'amIPlaying'),
+
+  playerOne: function () {
+    return this.get('players')[0];
+  }.property('players.@each'),
+
+  playerOneAvatarUrl: function () {
+    var playerOne = this.get('playerOne');
+    if (playerOne) {
+      return playerOne.get('user.avatarUrl');
+    } else {
+      return 'http://www.gravatar.com/avatar/?s=256&default=mm';
+    }
+  }.property('playerOne'),
+
+  playerTwo: function () {
+    return this.get('players')[1];
+  }.property('players.@each'),
+
+  playerTwoAvatarUrl: function () {
+    var playerTwo = this.get('playerTwo');
+    if (playerTwo) {
+      return playerTwo.get('user.avatarUrl');
+    } else {
+      return 'http://www.gravatar.com/avatar/?s=256&default=mm';
+    }
+  }.property('playerTwo'),
+
+  /** @property {Boolean} Is there at least one open seat? */
+  isWaitingForOpponent: function () {
+    var players = this.get('model.gameParticipants').filterBy('isPlaying');
+    return players.length < 2;
+  }.property('model.gameParticipants.@each.isPlaying'),
+
+  /** @property {String} The title showing on the top half of the board. */
+  topBoardTitle: function () {
+    var isWaitingForOpponent = this.get('isWaitingForOpponent');
+    var isGameInPrep = this.get('isGameInPrep');
+    var isGameOver = this.get('isGameOver');
+    var isGameInProgress = this.get('isGameInProgress');
+    var username = this.get('playerTwo.username.name');
+
+    if (isGameInPrep && isWaitingForOpponent) {
+      return 'Waiting for player...';
+    } else if (isGameInPrep && !isWaitingForOpponent) {
+      return `Waiting for ${username} to choose a deck.`;
+    }
+  }.property(
+    'isWaitingForOpponent',
+    'isGameInPrep',
+    'isGameOver',
+    'isGameInProgress'),
+
+  /** @property {String} The title showing on the bottom half of the board. */
+  bottomBoardTitle: function () {
+    var isWaitingForOpponent = this.get('isWaitingForOpponent');
+    var isGameInPrep = this.get('isGameInPrep');
+    var isGameOver = this.get('isGameOver');
+    var isGameInProgress = this.get('isGameInProgress');
+    var amIPlaying = this.get('amIPlaying');
+    var username = this.get('playerOne.user.username');
+
+    if (isGameInPrep && amIPlaying) {
+      return 'Choose a deck';
+    } else if (isGameInPrep && !amIPlaying) {
+      return `Waiting for ${username} to choose a deck.`;
+    }
+  }.property(
+    'isWaitingForOpponent',
+    'isGameInPrep',
+    'isGameOver',
+    'isGameInProgress',
+    'amIPlaying'),
 
   actions: {
     /**
