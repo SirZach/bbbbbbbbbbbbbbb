7cd75678baa0487b7302024cf6aaf0207379c0a3
ahaurw01 <aaron.haurwitz@gmail.com>
2015-04-25 00:46:52 -0400

    style up the mana curve chart

--- app/components/lil-card-stack.js
+++ app/components/lil-card-stack.js
@@ -7,13 +7,39 @@ export default Ember.Component.extend({
   classNames: ['lil-card-stack'],
 
   /** @property {Array<Card>} a list of card copies (identical cards) */
-  copies: function adsf () {
+  copies: function () {
     var card = this.get('cardGroup.card');
     var count = this.get('cardGroup.count');
+    var zIndex = this.get('startingZIndex');
     var copies = [];
     for (var i = 0; i < count; i++) {
-      copies.push(card);
+      copies.pushObject(Ember.Object.create({
+        card,
+        zIndex: zIndex--
+      }));
     }
     return copies;
-  }.property('cardGroup.card.name', 'cardGroup.count')
+  }.property('cardGroup.card.name', 'cardGroup.count'),
+
+  /** @property {Number} the number of cards in all related card groups */
+  totalCardsInCardGroups: function () {
+    return this.get('cardGroups')
+      .mapBy('count')
+      .reduce((acc, cur) => acc + cur);
+  }.property('cardGroups.@each.count'),
+
+  /** @property {Number} the z-index to start at for proper overlapping */
+  startingZIndex: function () {
+    var totalCardsInCardGroups = this.get('totalCardsInCardGroups');
+    var cardGroups = this.get('cardGroups');
+    var thisCardGroup = this.get('cardGroup');
+    var cardGroupIndex = 0;
+    var cardGroup = cardGroups[cardGroupIndex];
+    var startingZIndex = totalCardsInCardGroups;
+    while (cardGroup !== thisCardGroup) {
+      startingZIndex -= cardGroup.get('count');
+      cardGroup = cardGroups[++cardGroupIndex];
+    }
+    return startingZIndex;
+  }.property('totalCardsInCardGroups')
 });
--- app/components/lil-card.js
+++ app/components/lil-card.js
@@ -6,9 +6,11 @@ export default Ember.Component.extend({
 
   classNameBindings: [':lil-card', 'color'],
 
+  attributeBindings: ['style'],
+
   /** @property {String} color class */
   color: function () {
-    var colors = this.get('card.colors');
+    var colors = this.get('cardInfo.card.colors');
     if (!colors || !colors.length) {
       return 'lil-card-colorless';
     }
@@ -16,5 +18,11 @@ export default Ember.Component.extend({
       return `lil-card-${colors[0].toLowerCase()}`;
     }
     return `lil-card-multicolored`;
-  }.property('card.colors')
+  }.property('cardInfo.card.colors'),
+
+  /** @property {String} z-index for proper stacking */
+  style: function () {
+    var zIndex = this.get('cardInfo.zIndex');
+    return `z-index: ${zIndex};`;
+  }.property('cardInfo.zIndex')
 });
