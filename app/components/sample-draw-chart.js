9d0b5fac83f837c88acb954dfb13a4716a47d10b
ahaurw01 <aaron.haurwitz@gmail.com>
2015-05-02 09:38:26 -0400

    get the sample draw rendering in a nice cascade

--- app/components/sample-draw-chart.js
+++ app/components/sample-draw-chart.js
@@ -1,6 +1,38 @@
 import Ember from 'ember';
 import layout from '../templates/components/sample-draw-chart';
+import PaperCard from 'ember-paper/components/paper-card';
 
-export default Ember.Component.extend({
-  layout: layout
+
+export default PaperCard.extend({
+  layout,
+
+  fullscreen: false,
+
+  willInsertElement: function () {
+    this.send('refresh');
+  },
+
+  actions: {
+    /**
+     * "Draw" a new hand by choosing 7 random cards from the deck.
+     */
+    refresh: function () {
+      var cardGroups = this.get('deck.mainCardGroups');
+      var cards = [];
+      cardGroups.forEach((cardGroup) => {
+        for (var i = 0; i < cardGroup.get('count'); i++) {
+          cards.push(cardGroup.get('card'));
+        }
+      });
+      cards.shuffle();
+      // The hand is the first 7 cards.
+      this.set('hand', cards.slice(0, 7));
+      // The library is the 8th card to the end of the array.
+      this.set('library', cards.slice(7));
+    },
+
+    fullscreen: function () {
+      this.toggleProperty('fullscreen');
+    }
+  }
 });
