df6d395884bf49a693625777b96f2240c43b9e03
ahaurw01 <aaron.haurwitz@gmail.com>
2015-05-02 11:41:46 -0400

    clean up and test the sample draw component

--- app/components/sample-draw-chart.js
+++ app/components/sample-draw-chart.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import layout from '../templates/components/sample-draw-chart';
 import PaperCard from 'ember-paper/components/paper-card';
-
+import shuffle from '../utils/shuffle';
 
 export default PaperCard.extend({
   layout,
@@ -23,14 +23,14 @@ export default PaperCard.extend({
      * "Draw" a new hand by choosing 7 random cards from the deck.
      */
     refresh: function () {
-      var cardGroups = this.get('deck.mainCardGroups');
+      var cardGroups = this.get('deck.mainCardGroups') || [];
       var cards = [];
       cardGroups.forEach((cardGroup) => {
         for (var i = 0; i < cardGroup.get('count'); i++) {
           cards.push(cardGroup.get('card'));
         }
       });
-      cards.shuffle();
+      cards = shuffle(cards);
       // The hand is the first 7 cards.
       this.set('hand', cards.slice(0, 7));
       // The library is the 8th card to the end of the array.
--- tests/unit/components/sample-draw-chart-test.js
+++ tests/unit/components/sample-draw-chart-test.js
@@ -2,10 +2,11 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForComponent('sample-draw-chart', {
   // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:paper-card-content', 'component:paper-icon']
 });
 
 test('it renders', function(assert) {
@@ -18,4 +19,95 @@ test('it renders', function(assert) {
   // renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
+});
+
+var deckFixture = Ember.Object.create({
+  mainCardGroups: [
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Chalice of Life',
+        cmc: 3
+      })
+    }),
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Guttersnipe',
+        cmc: 3
+      })
+    }),
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Ajani\'s Pridemate',
+        cmc: 2
+      })
+    }),
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Lightning Strike',
+        cmc: 2
+      })
+    }),
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Soul Warden',
+        cmc: 1
+      })
+    }),
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Lightning Bolt',
+        cmc: 1
+      })
+    }),
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Plains',
+        cmc: 0
+      })
+    }),
+    Ember.Object.create({
+      count: 1,
+      card: Ember.Object.create({
+        name: 'Mountain',
+        cmc: 0
+      })
+    })
+  ]
+});
+
+test('hand has 7 cards', function (assert) {
+  var component = this.subject({
+    deck: deckFixture
+  });
+  component.send('refresh');
+  var hand = component.get('hand');
+  assert.equal(hand.length, 7);
+});
+
+test('sorted hand is sorted by name and cmc', function (assert) {
+  assert.expect(15);
+  var component = this.subject({
+    deck: deckFixture
+  });
+  component.send('refresh');
+  var hand = component.get('sortedHand');
+  assert.equal(hand.length, 7);
+  var previousCmc = -1;
+  var previousName = '';
+  hand.forEach((card) => {
+    assert.ok(card.get('cmc') >= previousCmc);
+    if (previousCmc !== card.get('cmc')) {
+      previousName = '';
+    }
+    assert.ok(card.get('name') >= previousName);
+    previousCmc = card.get('cmc');
+    previousName = card.get('name');
+  });
 });
