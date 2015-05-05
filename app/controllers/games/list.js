f82b9738cb6c0d7160ec1d728438e43c33686fd5
sirzach <zach.mcgonigal@gmail.com>
2015-05-04 23:04:35 -0400

    tests

--- app/controllers/games/list.js
+++ app/controllers/games/list.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   /** @property {Boolean} A combo of being signed up and having decks ready. */
   canCreateGame: function () {
-    return this.get('session.user');
+    return !Ember.isNone(this.get('session.user'));
   }.property('session.user'),
 
   cannotCreateGame: Ember.computed.not('canCreateGame')
