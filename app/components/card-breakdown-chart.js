bd9431a575597fc8aeeac09ad8bc9085344e9205
sirzach <zach.mcgonigal@gmail.com>
2015-05-07 23:42:36 -0400

    tests and handling corner cases of card types that are atypical

--- app/components/card-breakdown-chart.js
+++ app/components/card-breakdown-chart.js
@@ -38,19 +38,25 @@ export default Ember.Component.extend({
     cardGroups
       .filterBy('board', 'main')
       .forEach((group) => {
-        var stat = stats[group.get('card.mainType')];
+        var mainType = group.get('card.mainType');
+        if (!stats[mainType]) {
+          stats[mainType] = {
+            count: 0,
+            width: 0
+          };
+        }
+        var stat = stats[mainType];
         var count = group.get('count');
         stat.count += count;
         stat.width += (100 / mainCount) * count;
       });
 
-    return [
-      {label: 'Creature', count: stats.Creature.count, width: `${stats.Creature.width}%`},
-      {label: 'Artifact', count: stats.Artifact.count, width: `${stats.Artifact.width}%`},
-      {label: 'Sorcery', count: stats.Sorcery.count, width: `${stats.Sorcery.width}%`},
-      {label: 'Instant', count: stats.Instant.count, width: `${stats.Instant.width}%`},
-      {label: 'Land', count: stats.Land.count, width: `${stats.Land.width}%`},
-      {label: 'Enchantment', count: stats.Enchantment.count, width: `${stats.Enchantment.width}%`}
-    ];
+    return Ember.keys(stats).map((cardType) => {
+      return {
+        label: cardType,
+        count: stats[cardType].count,
+        width: `${stats[cardType].width}%`
+      };
+    });
   }.property('deck')
 });
--- tests/unit/components/card-breakdown-chart-test.js
+++ tests/unit/components/card-breakdown-chart-test.js
@@ -2,17 +2,22 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForComponent('card-breakdown-chart', {
   // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:az-graph', 'component:az-y', 'component:az-bars']
 });
 
 test('it renders', function(assert) {
   assert.expect(2);
 
   // creates the component instance
-  var component = this.subject();
+  var component = this.subject({
+    deck: Ember.Object.create({
+      cardGroups: []
+    })
+  });
   assert.equal(component._state, 'preRender');
 
   // renders the component to the page
