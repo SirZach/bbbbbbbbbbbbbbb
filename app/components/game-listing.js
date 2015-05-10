1fe48df463ba132acdc8360465fc1e4fe8f73ce5
sirzach <zach.mcgonigal@gmail.com>
2015-05-10 01:10:00 -0400

    allowing the user to delete a game if they own and there are no other players that have joined as isPlaying

--- app/components/game-listing.js
+++ app/components/game-listing.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 import layout from '../templates/components/game-listing';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+
+  canDeleteGame: function () {
+    var playerOne = this.get('game.playerOne');
+    var currentUser = this.get('currentUser');
+    var players = this.get('game.gameParticipants').filterBy('isPlaying');
+
+    return players.get('length') < 2 && playerOne.get('user.id') === currentUser;
+  }.property('game.playerOne.user.id', 'currentUser', 'game.gameParticipants.[].isPlaying'),
+
+  actions: {
+    deleteGame: function (game) {
+      this.sendAction('deleteGame', game);
+    }
+  }
 });
