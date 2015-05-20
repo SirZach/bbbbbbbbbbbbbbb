72ed87bc30990d783260286c7fc2642f1745bac9
sirzach <zach.mcgonigal@gmail.com>
2015-05-19 23:50:02 -0400

    can close context-menu when you click outside of the menu

--- app/components/context-menu.js
+++ app/components/context-menu.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import layout from '../templates/components/context-menu';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
   isOpen: false,
@@ -12,5 +13,21 @@ export default Ember.Component.extend({
     toggle: function () {
       this.toggleProperty('isOpen');
     }
-  }
+  },
+
+  clickHandler: function (event) {
+    if (this.get('isOpen')) {
+      if (!$(event.target).closest(`#${this.get('id')}`).length) {
+        this.send('toggle');
+      }
+    }
+  },
+
+  attachCloseout: function () {
+    $(document).on(`click.context-menu-${this.get('id')}`, this.clickHandler.bind(this));
+  }.on('didInsertElement'),
+
+  removeDocListener: function () {
+    $(document).off(`click.context-menu-${this.get('id')}`);
+  }.on('willDestroyElement')
 });
