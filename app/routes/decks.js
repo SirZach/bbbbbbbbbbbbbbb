ed4368d3729bd8be46e13ef64c1ccb699908ec01
ahaurw01 <aaron.haurwitz@gmail.com>
2015-05-25 09:04:24 -0400

    lazy load all decks on the decks/list page

--- app/routes/decks.js
+++ app/routes/decks.js
@@ -8,7 +8,34 @@ export default Ember.Route.extend({
   },
 
   model: function () {
-    return this.get('session.user').then((user) => user.get('decks'));
+    return this._createFetchPromise();
+  },
+
+  _createFetchPromise: function () {
+    // Adding a space to the last deck id is a hack to tell firebase to start
+    // AFTER the last id we saw.
+    //
+    var startAt = (this.get('_lastDeckId') || '') + ' ';
+    var limitToFirst = 5;
+    return this.store.find('deck', {
+      startAt,
+      limitToFirst
+    }).then((decks) => {
+      var model = this.currentModel || [];
+      decks = decks.toArray();
+      if (decks.length === 0 || decks.length < limitToFirst) {
+        // Setting `reachedInfinity` on the model tells the infinity loader to
+        // style itself propertly. It doesn't, however, stop it from firing load
+        // events.
+        //
+        model.set('reachedInfinity', true);
+      } else {
+        // Stash the last id so we know where to start next.
+        this.set('_lastDeckId', decks.get('lastObject.id'));
+      }
+      model.pushObjects(decks);
+      return model;
+    });
   },
 
   actions: {
@@ -40,6 +67,15 @@ export default Ember.Route.extend({
 
     importDeck: function () {
       this.send('openModal', 'decks/import-deck');
+    },
+
+    infinityLoad: function () {
+      var model = this.currentModel;
+      if (this.get('_isLoading') || model.get('reachedInfinity')) {
+        return;
+      }
+      this.set('_isLoading', true);
+      this._createFetchPromise().then(() => this.set('_isLoading', false));
     }
   }
 });
