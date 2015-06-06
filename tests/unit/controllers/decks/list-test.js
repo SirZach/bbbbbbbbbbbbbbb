86ee122ed833d900b54bf56ac4808412206ce9a3
ahaurw01 <aaron.haurwitz@gmail.com>
2015-06-06 10:10:35 -0400

    fix decks list controller test

--- tests/unit/controllers/decks/list-test.js
+++ tests/unit/controllers/decks/list-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleFor('controller:decks/list', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['controller:decks']
 });
 
 // Replace this with your real tests.
