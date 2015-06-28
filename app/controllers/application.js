cda0f492171dfaeb38c8dd03673e27c29b6a3941
ahaurw01 <aaron.haurwitz@gmail.com>
2015-06-28 09:12:20 -0400

    add class to sidenav on certain routes

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,5 +5,17 @@ export default Ember.Controller.extend({
 
   routeDidChange: function () {
     this.set('drawerOpen', false);
-  }.observes('currentPath')
+  }.observes('currentPath'),
+
+  forceSidenavUnlocked: Ember.computed('currentPath', function () {
+    return this.get('currentPath') === 'games.game';
+  }),
+
+  sidenavClassnames: Ember.computed('forceSidenavUnlocked', function () {
+    var classNames = 'md-sidenav-left md-whiteframe-z2';
+    if (this.get('forceSidenavUnlocked')) {
+      classNames += ' force-unlocked';
+    }
+    return classNames;
+  })
 });
