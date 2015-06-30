fda32119d7e7a17155420a5e00295355291bb540
ahaurw01 <aaron.haurwitz@gmail.com>
2015-06-30 07:42:47 -0400

    add magnifying glass over card image

--- app/components/game-card.js
+++ app/components/game-card.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 import layout from '../templates/components/game-card';
 
+var MAGNIFYING_GLASS_WIDTH = 250;
+var MAGNIFYING_GLASS_HEIGHT = 250;
+
 export default Ember.Component.extend({
   layout: layout,
 
@@ -56,5 +59,39 @@ export default Ember.Component.extend({
     if (this.get('canTap') && !this.get('readOnly')) {
       this.sendAction('tap', this.get('gameCard'));
     }
+  },
+
+  mouseEnter: function (event) {
+    // Calculate the bounds.
+    var $img = $(event.target);
+    this.set('magnifyMinX', $img.offset().left);
+    this.set('magnifyMaxX', $img.offset().left + $img.width());
+    this.set('magnifyMinY', $img.offset().top);
+    this.set('magnifyMaxY', $img.offset().top + $img.height());
+    this.set('isMagnifying', true);
+  },
+
+  mouseMove: function (event) {
+    // See if we are still over the image.
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+    if (mouseX < this.get('magnifyMinX') ||
+        mouseX > this.get('magnifyMaxX') ||
+        mouseY < this.get('magnifyMinY') ||
+        mouseY > this.get('magnifyMaxY')) {
+      this.set('isMagnifying', false);
+      return;
+    }
+    // Top and left coordinates for the placement of the magnifying glass.
+    var left = mouseX - MAGNIFYING_GLASS_WIDTH / 2;
+    var top = mouseY - MAGNIFYING_GLASS_HEIGHT / 2;
+    this.set('magnifyLeft', left);
+    this.set('magnifyTop', top);
+
+    // X and Y coordinates for the inner position of the background image.
+  },
+
+  mouseLeave: function () {
+    this.set('isMagnifying', false);
   }
 });
