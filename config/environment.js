2c6af89ecbee09e3eaf47da357580403d6e9c5d9
ahaurw01 <aaron.haurwitz@gmail.com>
2015-07-01 23:45:37 -0400

    hook up the firebase dev instance

--- config/environment.js
+++ config/environment.js
@@ -31,10 +31,11 @@ module.exports = function(environment) {
       'img-src': "'self' http://localhost:3000 https://avatars.githubusercontent.com https://www.gravatar.com http://avatars.io http://pbs.twimg.com",
       'font-src': "https://fonts.gstatic.com http://localhost:4200 http://www.fontsaddict.com",
       'style-src': "'self' 'unsafe-inline'",
-      'connect-src': "'self' http://localhost:3000 https://s-dal5-nss-18.firebaseio.com wss://s-dal5-nss-18.firebaseio.com https://api.app.netuitive.com",
+      'connect-src': "'self' http://localhost:3000 https://*.firebaseio.com wss://*.firebaseio.com https://api.app.netuitive.com",
       'report-uri': "'none'",
       'script-src': "'self' 'unsafe-eval' localhost:35729 0.0.0.0:35729 https://assets.app.netuitive.com"
     };
+    ENV.firebase = 'https://dim-glow-0245.firebaseio.com/';
   }
 
   if (environment === 'test') {
