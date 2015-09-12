6aa35d03443dc0ec61fdb97d272871231741ca29
sirzach <zach.mcgonigal@gmail.com>
2015-09-11 21:59:37 -0400

    not showing the last divider on the games list page. also trying out some new es6 stuff

---
+++ app/components/is-last.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+const { get, computed } = Ember;
+
+export default Ember.Component.extend({
+  /** @property {Array} the array of iterables */
+  content: [],
+
+  /** @property {Number} index of current iterable */
+  index: null,
+
+  /** @property {Boolean} is current iterable last in the content */
+  isLast: computed('index', 'content', function () {
+    let content = get(this, 'content');
+    let index = get(this, 'index');
+
+    return index === (get(content, 'length') - 1);
+  }),
+
+  notLast: computed.not('isLast')
+});
