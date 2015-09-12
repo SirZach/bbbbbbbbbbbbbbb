a738829cefa5dc884017728e40e32767c3cd510d
ahaurw01 <aaron.haurwitz@gmail.com>
2015-09-12 11:39:47 -0400

    replace x-paper-text in /chat/*

--- app/controllers/chat/channel.js
+++ app/controllers/chat/channel.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  /** @property {String} bound to the chat input */
+  says: null,
+
+  /** @property {String} set by the route */
+  channel: null,
+
   sortedPresences: function () {
     var presences = this.get('presences');
     if (!presences) {
@@ -11,5 +17,14 @@ export default Ember.Controller.extend({
       sortAscending: true,
       content: presences.get('content')
     });
-  }.property('presences.@each.state', 'presences.@each.user.username')
+  }.property('presences.@each.state', 'presences.@each.user.username'),
+
+  actions: {
+    submitChat() {
+      let says = this.get('says');
+      let channel = this.get('channel');
+      this.send('say', says, channel);
+      this.set('says', null);
+    }
+  }
 });
