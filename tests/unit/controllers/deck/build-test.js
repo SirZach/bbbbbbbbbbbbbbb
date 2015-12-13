53b4718e31674f983544985937e76fe395c4ba37
ahaurw01 <aaron.haurwitz@gmail.com>
2015-12-12 19:00:49 -0500

    test the default deck image url

--- tests/unit/controllers/deck/build-test.js
+++ tests/unit/controllers/deck/build-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:deck/build');
@@ -22,4 +23,13 @@ test('defaultImageUrl is from the most expensive card', function(assert) {
   let result = controller.get('defaultImageUrl');
 
   assert.equal(result, '/Titanic%20Growth');
+});
+
+test('defaultImageUrl is default if no cards', function(assert) {
+  let controller = this.subject();
+
+  let result = controller.get('defaultImageUrl');
+
+  assert.equal(result,
+    'http://big-furry-monster.herokuapp.com/images/default');
 });
