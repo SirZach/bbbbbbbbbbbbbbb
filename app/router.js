314de2c5f99e177bf45e64b424051362121d51b6
sirzach <zach.mcgonigal@gmail.com>
2015-12-20 15:33:46 -0500

    password recovery. for #208

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ let Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
+  this.route('password-reset');
   this.route('disclaimer');
   this.resource('user', { path: '/u/:user_id' });
   this.resource('decks', function() {
