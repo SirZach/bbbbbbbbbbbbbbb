7ceba274632bcc3d5d5b66fe3830e8726f53d194
sirzach <zach.mcgonigal@gmail.com>
2016-01-18 10:32:15 -0500

    i always expect the scope of these upgrades to be much smaller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,24 +4,27 @@ const { inject, computed } = Ember;
 export default Ember.Controller.extend({
   userController: inject.controller('user'),
 
-  routeDidChange: function() {
-    this.set('drawerOpen', false);
-  }.observes('currentPath'),
-
   /**
-   * @property {Boolean}
-   * Should the side nav be unlocked regardless of viewport width?
-   */
-  forceSidenavUnlocked: computed('currentPath', function() {
-    return this.get('currentPath') === 'game';
-  }),
-
-  /** @property {Boolean} Classes to apply to the side nav container. */
-  navContainerClassnames: computed('forceSidenavUnlocked', function() {
-    let classNames = 'ember-app';
-    if (this.get('forceSidenavUnlocked')) {
-      classNames += ' force-unlocked';
-    }
-    return classNames;
-  })
+    We may need this code again as 
+  */
+  // routeDidChange: function() {
+  //   this.set('drawerOpen', false);
+  // }.observes('currentPath'),
+  //
+  // /**
+  //  * @property {Boolean}
+  //  * Should the side nav be unlocked regardless of viewport width?
+  //  */
+  // forceSidenavUnlocked: computed('currentPath', function() {
+  //   return this.get('currentPath') === 'game';
+  // }),
+  //
+  // /** @property {Boolean} Classes to apply to the side nav container. */
+  // navContainerClassnames: computed('forceSidenavUnlocked', function() {
+  //   let classNames = 'ember-app';
+  //   if (this.get('forceSidenavUnlocked')) {
+  //     classNames += ' force-unlocked';
+  //   }
+  //   return classNames;
+  // })
 });
